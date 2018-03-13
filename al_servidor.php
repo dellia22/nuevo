658a184d1701462adcd66b1ef97ddb80b05ffbbf
Laura <laura@microbiologia.com>
2018-03-13 12:25:05 +0000

    confirmado

---
+++ al_servidor.php
@@ -0,0 +1,79 @@
+<?php
+
+##### http://www.lawebdelprogramador.com #####
+
+ 
+
+# Comprobamos que se haya enviado algo desde el formulario
+
+if(is_uploaded_file($_FILES["archivo"]["tmp_name"]))
+
+{
+
+	# Definimos las variables
+
+	$host="fresita.microbiologia.ull.es";
+
+	$port=80;
+
+	$user="laura";
+
+	$password="141981";
+
+	$ruta="";
+
+ 
+
+	# Realizamos la conexion con el servidor
+
+	$conn_id=@ftp_connect($host,$port);
+
+	if($conn_id)
+
+	{
+
+		# Realizamos el login con nuestro usuario y contraseña
+
+		if(@ftp_login($conn_id,$user,$password))
+
+		{
+
+			# Cambiamos al directorio especificado
+
+			if(@ftp_chdir($conn_id,$ruta))
+
+			{
+
+				# Subimos el fichero
+
+				if(@ftp_put($conn_id,$_FILES["archivo"]["name"],$_FILES["archivo"]["tmp_name"],FTP_BINARY))
+
+					echo "Fichero subido correctamente";
+
+				else
+
+					echo "No ha sido posible subir el fichero";
+
+			}else
+
+				echo "No existe el directorio especificado";
+
+		}else
+
+			echo "El usuario o la contraseña son incorrectos";
+
+		# Cerramos la conexion ftp
+
+		ftp_close($conn_id);
+
+	}else
+
+		echo "No ha sido posible conectar con el servidor";
+
+}else{
+
+   echo "Selecciona un archivo...";
+
+}
+
+?>
